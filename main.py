b8a15d0f265214aef349d0a28013aa69d277c94d
sneakykiwi <59621197+sneakykiwi@users.noreply.github.com>
2020-06-25 15:44:14 +0300

    Update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord, os
 from discord.ext import commands
-from coloroma import init
+from colorama import init
 from termcolor import colored
 
 
@@ -21,6 +21,6 @@ async def on_ready():
         f'Invite URL: {discord.utils.oauth_url(client.user.id)}&permissions=8\n', 'green'))
     await client.change_presence(activity=discord.Activity(name=f"Made by Kiwi!", type=discord.ActivityType.listening))
 
-Autorole.setup(client=client, guild_id=725474351440199680, member_role_id=725681956804886529, welcome_channel_id=725474351440199684)
+Autorole.setup(client=client, member_role_id=725681956804886529, welcome_channel_id=725474351440199684)
 
 client.run(os.getenv('CLIENT_TOKEN'))
